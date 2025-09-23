bc4d3a7abb33d0f2cc69d884b1b68828ca8c0293
Sanchopanzos <alexosnach73@gmail.com>
2025-09-23 11:15:16 +0500

    layout

--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
 console.log("Hello, New Eridu");
+const cards = document.querySelectorAll(".card");
+for (let card of cards){
+    card.addEventListener("click", ()=>{
+        card.classList.add("card-show");
+    })
+}
