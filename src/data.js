be86f068a5dcb74c2e2d85611eb26689ac8dafee
Sanchopanzos <alexosnach73@gmail.com>
2025-09-30 10:56:33 +0500

    addJs

---
+++ src/data.js
@@ -0,0 +1,70 @@
+import img1 from './img/belobog.jpg'
+import img2 from './img/Calydon.jpg'
+import img3 from './img/HSOS6.jpg'
+import img4 from './img/OBOL.jpg'
+import img5 from './img/PubSec.jpg'
+import img6 from './img/CN.jpg'
+
+const images = [
+    {
+        "id" : 1,
+        "url": img1
+    },
+    
+    {
+        "id" : 2,
+        "url": img2
+    },
+
+    {
+        "id" : 3,
+        "url": img3
+    },
+
+    {
+        "id" : 4,
+        "url": img4
+    },
+            
+    {
+        "id" : 5,
+        "url": img5
+    },
+
+        {
+        "id" : 6,
+        "url": img1
+    },
+    
+    {
+        "id" : 7,
+        "url": img2
+    },
+
+    {
+        "id" : 8,
+        "url": img3
+    },
+
+    {
+        "id" : 9,
+        "url": img4
+    },
+            
+    {
+        "id" : 10,
+        "url": img5
+    },
+        
+    {
+        "id" : 11,
+        "url": img6
+    },
+        
+    {
+        "id" : 12,
+        "url": img6
+    }
+];
+
+export {images}
--- src/main.js
+++ src/main.js
@@ -1,7 +1,24 @@
+import {images} from "./data.js"
+
 console.log("Hello, New Eridu");
-const cards = document.querySelectorAll(".card");
-for (let card of cards){
+
+const container = document.querySelector(".container"); // находит на странице элемент с классом container
+
+images.forEach((item) => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    
+    const img = document.createElement("img");
+    img.src = item.url;
+    img.alt = "";
+
+    card.append(img);
+    container.append(card);
+
     card.addEventListener("click", ()=>{
-        card.classList.add("card-show");
+    card.classList.toggle("card-show");
+    setTimeout(() => {
+        card.classList.toggle("card-show");
+    }, 3000);
     })
-}
+});
