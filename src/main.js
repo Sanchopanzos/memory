2d0da0b4b2e1001d29238b86e2f46ab493ce2990
Sanchopanzos <alexosnach73@gmail.com>
2025-09-30 11:11:56 +0500

    max2cards

--- src/main.js
+++ src/main.js
@@ -1,13 +1,18 @@
-import {images} from "./data.js"
+import { images } from "./data.js"
+
+images.sort(() => Math.random() - 0.5);
 
 console.log("Hello, New Eridu");
 
 const container = document.querySelector(".container"); // находит на странице элемент с классом container
 
+let visibleCards = [];
+
 images.forEach((item) => {
     const card = document.createElement("div");
     card.classList.add("card");
-    
+    card.classList.add("card" + item.id)
+
     const img = document.createElement("img");
     img.src = item.url;
     img.alt = "";
@@ -15,10 +20,20 @@ images.forEach((item) => {
     card.append(img);
     container.append(card);
 
-    card.addEventListener("click", ()=>{
-    card.classList.toggle("card-show");
-    setTimeout(() => {
-        card.classList.toggle("card-show");
-    }, 3000);
+    card.addEventListener("click", () => {
+        if (visibleCards.length < 2) {
+            card.classList.toggle("card-show");
+
+            visibleCards.push(item);
+
+            if(visibleCards.length == 2)
+            {
+                setTimeout(() => {
+                    card.classList.toggle("card-show");
+                    document.querySelector(".card"+visibleCards[0].id).classList.remove("card-show"); 
+                    visibleCards = [];
+                }, 3000);
+            }
+        }
     })
 });
